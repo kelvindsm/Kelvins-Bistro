7a7cdc91f61f98fb04b6190a727eb3387f87d2cb
Kelvin Martins <kelvin.martins5@outlook.com>
2026-05-29 21:19:49 -0300

    Add database creation and use statements

--- init.sql
+++ init.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS kelvinbistrodb;
+USE kelvinbistrodb;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
